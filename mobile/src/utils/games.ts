b7eb830149578a455151fd158cd863a4dadf93fa
Roberto Costa <roberto.araujo339@gmail.com>
2022-09-18 19:28:25 -0300

    Mobile first page finished

--- mobile/src/utils/games.ts
+++ mobile/src/utils/games.ts
@@ -1,38 +1,56 @@
 export const GAMES = [
   {
+    _count: {
+      Ad: 4,
+    },
     id: '1',
-    name: 'League of Legends',
+    title: 'League of Legends',
     ads: '4',
-    cover: require('../assets/games/game-1.png')
+    bannerUrl: require('../assets/games/game-1.png'),
   },
   {
+    _count: {
+      Ad: 4,
+    },
     id: '2',
-    name: 'Dota 2',
+    title: 'Dota 2',
     ads: '3',
-    cover: require('../assets/games/game-2.png')
+    bannerUrl: require('../assets/games/game-2.png'),
   },
   {
+    _count: {
+      Ad: 4,
+    },
     id: '3',
-    name: 'CS-GO',
+    title: 'CS-GO',
     ads: '2',
-    cover: require('../assets/games/game-3.png')
+    bannerUrl: require('../assets/games/game-3.png'),
   },
   {
+    _count: {
+      Ad: 4,
+    },
     id: '4',
-    name: 'Apex',
+    title: 'Apex',
     ads: '3',
-    cover: require('../assets/games/game-4.png')
+    bannerUrl: require('../assets/games/game-4.png'),
   },
   {
+    _count: {
+      Ad: 4,
+    },
     id: '5',
-    name: 'Fortnite',
+    title: 'Fortnite',
     ads: '5',
-    cover: require('../assets/games/game-5.png')
+    bannerUrl: require('../assets/games/game-5.png'),
   },
   {
+    _count: {
+      Ad: 4,
+    },
     id: '6',
-    name: 'World of Warcraft',
+    title: 'World of Warcraft',
     ads: '2',
-    cover: require('../assets/games/game-6.png')
+    bannerUrl: require('../assets/games/game-6.png'),
   },
 ];
